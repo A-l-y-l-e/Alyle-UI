e1453885601c54fc11f1d0b22458d09fd7ff8d86
alyleui <alyleui@gmail.com>
2017-07-16 21:52:51 -0500

    new examples for button

---
+++ src/app/button-demo/example-01/example-01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'button-example-01',
+  templateUrl: './example-01.component.html',
+  styleUrls: ['./example-01.component.css']
+})
+export class Example01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
