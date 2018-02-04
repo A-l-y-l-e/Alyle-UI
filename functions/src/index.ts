a8656e9e0df162e87ca42b7d92847473753568cf
alyleui <alyleui@gmail.com>
2018-02-04 15:52:26 -0500

    update srr

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,11 +3,14 @@
 import 'zone.js/dist/zone-node';
 // tslint:disable-next-line:no-implicit-dependencies no-import-side-effect
 import 'reflect-metadata';
-import { enableProdMode } from '@angular/core';
+console.log(process.cwd());
 import * as functions from 'firebase-functions';
-
+const { enableProdMode } = require('@angular/core');
+const { renderModuleFactory } = require('@angular/platform-server');
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 import * as express from 'express';
-import { join } from 'path';
+import { join, resolve } from 'path';
+
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -20,17 +23,30 @@ const DIST_FOLDER = join(process.cwd(), './dist');
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('../dist-server/main.bundle');
 
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+// Import module map for lazy loading
+
+// app.engine('html', ngExpressEngine({
+//   bootstrap: AppServerModuleNgFactory,
+//   providers: [
+//     provideModuleMap(LAZY_MODULE_MAP)
+//   ]
+// }));
+
+const index = require('fs')
+.readFileSync((join(DIST_FOLDER, 'index.html')), 'utf8')
+.toString();
+
+
+app.get('**', (req, res) => {
+    renderModuleFactory(AppServerModuleNgFactory, {
+        url: req.path,
+        document: index,
+        extraProviders: [
+          provideModuleMap(LAZY_MODULE_MAP)
+        ]
+    }).then(html => res.status(200).send(html));
+});
 
 
 // TODO: implement data requests securely
@@ -39,7 +55,7 @@ app.get('/api/*', (req, res) => {
 });
 
 // All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render(join(DIST_FOLDER, 'index.html'), { req });
-});
+// app.get('*', (req, res) => {
+//   res.render(join(DIST_FOLDER, 'index.html'), { req });
+// });
 export const ssr = functions.https.onRequest(app);
