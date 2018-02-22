076017d334d0bbe466efcdba6b00a2de7752d4ac
alyleui <alyleui@gmail.com>
2018-02-22 10:00:31 -0500

    update routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,15 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { environment } from 'environments/environment';
 
 const routes: Routes = [
-  { path: '', loadChildren: './home/home.module#HomeModule' },
   { path: 'get-started', loadChildren: './components/get-started/get-started.module#GetStartedModule' },
   { path: 'theming', loadChildren: './components/theming/theming.module#ThemingModule' },
   { path: 'components', loadChildren: './components/index#ComponentsModule' },
-  { path: 'bg-color', loadChildren: './components/bg-color-demo/bg-color-demo.module#BgColorDemoModule' }
+  { path: 'bg-color', loadChildren: './components/bg-color-demo/bg-color-demo.module#BgColorDemoModule' },
+  { path: '', loadChildren: './home/home.module#HomeModule' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: !environment.production })],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
