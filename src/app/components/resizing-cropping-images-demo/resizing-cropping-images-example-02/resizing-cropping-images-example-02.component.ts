279ef30aff2c043a4240bca88931414082badb2d
alyleui <alyleui@gmail.com>
2018-04-04 23:59:00 -0500

    cropping: add examples

---
+++ src/app/components/resizing-cropping-images-demo/resizing-cropping-images-example-02/resizing-cropping-images-example-02.component.ts
@@ -0,0 +1,37 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation
+  } from '@angular/core';
+import {
+  CroppedImage,
+  ImageResolution,
+  LyResizingCroppingImages,
+  LyResizingCroppingImagesConfig
+  } from 'alyle-ui/resizing-cropping-images';
+
+@Component({
+  selector: 'resizing-cropping-images-example-02',
+  templateUrl: './resizing-cropping-images-example-02.component.html',
+  styleUrls: ['./resizing-cropping-images-example-02.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ResizingCroppingImagesExample02Component {
+
+  @ViewChild(LyResizingCroppingImages) img: LyResizingCroppingImages;
+  result: string;
+  myConfig: LyResizingCroppingImagesConfig = {
+    width: 150, // Default `250`
+    height: 150, // Default `200`,
+    output: ImageResolution.OriginalImage // Default ImageResolution.Default
+  };
+
+  constructor() { }
+
+  crop() {
+    const imgCropped: CroppedImage = this.img.crop();
+  }
+
+}
