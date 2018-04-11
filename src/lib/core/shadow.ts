6c06a7f37d3728cd94b2db594838be9a009680db
alyleui <alyleui@gmail.com>
2018-04-10 22:13:38 -0500

    fix(shadow): update shadow on change theme

---
+++ src/lib/core/shadow.ts
@@ -0,0 +1,45 @@
+import * as _chroma from 'chroma-js';
+const chroma = _chroma;
+
+const shadowKeyUmbraOpacity = 0.2;
+const shadowKeyPenumbraOpacity = 0.14;
+const shadowAmbientShadowOpacity = 0.12;
+const elevations = [
+  [0, 1, 3, 0, 0, 1, 1, 0, 0, 2, 1, -1],
+  [0, 1, 5, 0, 0, 2, 2, 0, 0, 3, 1, -2],
+  [0, 1, 8, 0, 0, 3, 4, 0, 0, 3, 3, -2],
+  [0, 2, 4, -1, 0, 4, 5, 0, 0, 1, 10, 0],
+  [0, 3, 5, -1, 0, 5, 8, 0, 0, 1, 14, 0],
+  [0, 3, 5, -1, 0, 6, 10, 0, 0, 1, 18, 0],
+  [0, 4, 5, -2, 0, 7, 10, 1, 0, 2, 16, 1],
+  [0, 5, 5, -3, 0, 8, 10, 1, 0, 3, 14, 2],
+  [0, 5, 6, -3, 0, 9, 12, 1, 0, 3, 16, 2],
+  [0, 6, 6, -3, 0, 10, 14, 1, 0, 4, 18, 3],
+  [0, 6, 7, -4, 0, 11, 15, 1, 0, 4, 20, 3],
+  [0, 7, 8, -4, 0, 12, 17, 2, 0, 5, 22, 4],
+  [0, 7, 8, -4, 0, 13, 19, 2, 0, 5, 24, 4],
+  [0, 7, 9, -4, 0, 14, 21, 2, 0, 5, 26, 4],
+  [0, 8, 9, -5, 0, 15, 22, 2, 0, 6, 28, 5],
+  [0, 8, 10, -5, 0, 16, 24, 2, 0, 6, 30, 5],
+  [0, 8, 11, -5, 0, 17, 26, 2, 0, 6, 32, 5],
+  [0, 9, 11, -5, 0, 18, 28, 2, 0, 7, 34, 6],
+  [0, 9, 12, -6, 0, 19, 29, 2, 0, 7, 36, 6],
+  [0, 10, 13, -6, 0, 20, 31, 3, 0, 8, 38, 7],
+  [0, 10, 13, -6, 0, 21, 33, 3, 0, 8, 40, 7],
+  [0, 10, 14, -6, 0, 22, 35, 3, 0, 8, 42, 7],
+  [0, 11, 14, -7, 0, 23, 36, 3, 0, 9, 44, 8],
+  [0, 11, 15, -7, 0, 24, 38, 3, 0, 9, 46, 8]
+];
+export function shadowBuilder(elevation: number | string, color?: string) {
+  const Color = chroma(color || '#000');
+  const colors = [
+    Color.alpha(shadowKeyUmbraOpacity).css(),
+    Color.alpha(shadowKeyPenumbraOpacity).css(),
+    Color.alpha(shadowAmbientShadowOpacity).css()
+  ];
+  const e = elevations[elevation];
+  return [
+    `box-shadow:${e[0]}px ${e[1]}px ${e[2]}px ${e[3]}px ${colors[0]},${e[4]}px ${e[5]}px ${e[6]}px ${e[7]}px ${colors[1]},${e[8]}px ${e[9]}px ${e[10]}px ${e[11]}px ${colors[2]}`,
+  ];
+
+}
--- src/lib/shadow/deep.ts
+++ src/lib/shadow/deep.ts
@@ -8,24 +8,49 @@ import {
   ModuleWithProviders,
   ElementRef,
   ViewContainerRef,
-  HostBinding
+  HostBinding,
+  OnInit,
+  Renderer2
 } from '@angular/core';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { LyShadowService } from './shadow.service';
 import { toPositiveNumber } from './toPositiveNumber';
-import { LyTheme } from 'alyle-ui/core';
+import { LyTheme, shadowBuilder } from 'alyle-ui/core';
 
 @Directive({
-  selector: '[ly-deep], [ly-shadow]',
+  selector: '[ly-deep], [ly-shadow], [lyShadow]'
 })
-export class LyDeepComponent {
+export class LyDeepComponent implements OnInit {
   private _size = 1;
   private _interval = 3000;
   private _result: string;
   private numberState = -1;
   private _shadowColor = 'rgba(0,0,0,0.87)';
+  private _classId: string;
+  /** Default elevation */
+  private elevation: string | number = 1;
   @HostBinding('style.box-shadow') styleBoxShadow: SafeStyle | string;
+  @Input()
+  set lyShadow(val: string[]) {
+    console.time('time createStyle');
+    let keys: string;
+    let elevation: string | number;
+    let color: string;
+    if (val && val[0]) {
+      keys = val.join();
+      elevation = val[0];
+      color = val[1];
+    } else {
+      keys = `${this.elevation}`;
+      elevation = this.elevation;
+    }
+    const newStyle = this.theme.createStyle(`ly-${keys}`, this.css.bind(this), elevation, color);
+    this.setClassName(newStyle.id, this._classId);
+    this._classId = keys;
+    console.timeEnd('time createStyle');
+  }
+
   @Input('shadowColor')
   set shadowColor(val: string) {
     this._shadowColor = this.theme.colorOf(val);
@@ -37,18 +62,36 @@ export class LyDeepComponent {
   constructor(
     private elementRef: ElementRef,
     private shadow: LyShadowService,
-    private theme: LyTheme
+    private theme: LyTheme,
+    private renderer: Renderer2
   ) {
 
   }
+
+  setClassName(newClass: string, oldClass?: string) {
+    this.renderer.addClass(this.elementRef.nativeElement, newClass);
+    if (oldClass) {
+      this.renderer.removeClass(this.elementRef.nativeElement, oldClass);
+    }
+  }
+
+  ngOnInit() {
+    // console.log('slect', this.styleSheet.existStyle(''));
+  }
+
   public styleHost() {
     this.styleBoxShadow = this.shadow.shadow(this.shadowColor, this.scale);
+  }
+
+  css(elevation: string | number, color: string) {
+    return `${shadowBuilder(elevation, this.theme.colorOf(color))}`;
   }
 
   @Input()
   public get scale(): number {
     return this._size;
   }
+
   public set scale(val: number) {
     this._size = val;
     // this.numberState = val >= 0 ? 1 : -1;
