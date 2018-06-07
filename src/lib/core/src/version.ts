61d5054908de9d06bfbe15bcd7561b95e1b5700a
alyleui <alyleui@gmail.com>
2018-06-07 10:44:55 -0500

    bump version

--- src/lib/core/src/version.ts
+++ src/lib/core/src/version.ts
@@ -1,2 +1,2 @@
-export const AUI_VERSION = '1.7.0-beta.14ozd';
-export const AUI_LAST_UPDATE = '2018-06-06T05:16:03.742Z';
+export const AUI_VERSION = '1.7.0-beta.17cr0';
+export const AUI_LAST_UPDATE = '2018-06-07T15:44:41.077Z';
