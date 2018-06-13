ee85deb2d4b89c5ea73ef54c781a0c61d29fdc4a
alyleui <alyleui@gmail.com>
2018-06-12 21:16:59 -0500

    tools: update

--- tools/src/build-lib.ts
+++ tools/src/build-lib.ts
@@ -2,6 +2,7 @@ import { allComponents } from './prepare-lib';
 import { spawnSync } from 'child_process';
 import { join } from 'path';
 import { copySync } from 'fs-extra';
+import { writeFileSync, readFileSync} from 'fs';
 
 /** build all */
 let previousBuild = Promise.resolve();
@@ -12,6 +13,33 @@ allComponents.forEach((lib) => {
     if (ls.status) {
       process.exit(1);
     }
-    copySync(join(`${process.cwd()}/dist`, lib.pkgName), `${process.cwd()}/dist/node_modules/${lib.pkgName}`);
+    const dirLib = join(`${process.cwd()}/dist`, lib.pkgName);
+    copySync(dirLib, `${process.cwd()}/dist/node_modules/${lib.pkgName}`);
+    /** for subModules */
+    const pkg = JSON.parse(readFileSync(join(dirLib, 'package.json')).toString());
+    const tempPkg = {
+      'name': pkg.name,
+      'sideEffects': false
+    };
+    [
+      'main',
+      'module',
+      'es2015',
+      'esm5',
+      'esm2015',
+      'fesm5',
+      'fesm2015',
+      'typings',
+      'metadata'
+    ].forEach(key => {
+      tempPkg[key] = `./${pkg[key]}`;
+    });
+    let newPkg;
+    if (lib.pkgName.split('/').length > 2) {
+      newPkg = tempPkg;
+    } else {
+      newPkg = Object.assign(pkg, tempPkg);
+    }
+    writeFileSync(join(`${process.cwd()}/dist`, lib.pkgName, 'package.json'), JSON.stringify(newPkg, undefined, 2), 'utf8');
   });
 });
