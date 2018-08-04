d631e7fb4c2e7fb14acf32b0a4590d07d80e7309
alyleui <alyleui@gmail.com>
2018-08-04 00:03:13 -0500

    docs(card): update demo with ly-card-media

--- src/app/docs/components/card-demo/card-demo-basic/card-demo-basic.module.ts
+++ src/app/docs/components/card-demo/card-demo-basic/card-demo-basic.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LyCommonModule } from '@alyle/ui';
 import { CardDemoBasicComponent } from './card-demo-basic.component';
 import { LyCardModule } from '@alyle/ui/card';
 import { LyTypographyModule } from '@alyle/ui/typography';
 import { LyButtonModule } from '@alyle/ui/button';
+import { LyIconButtonModule } from '@alyle/ui/icon-button';
+import { LyIconModule } from '@alyle/ui/icon';
 
 @NgModule({
   imports: [
     CommonModule,
+    LyCommonModule,
     LyCardModule,
     LyTypographyModule,
-    LyButtonModule
+    LyButtonModule,
+    LyIconButtonModule,
+    LyIconModule
   ],
   exports: [CardDemoBasicComponent],
   declarations: [CardDemoBasicComponent]
