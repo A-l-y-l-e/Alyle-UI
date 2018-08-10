ecc90e963b650eb5c38a0e72225d89febe152f1d
alyleui <alyleui@gmail.com>
2018-08-10 02:34:20 -0500

    fix(theme): fix circular dependency

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -24,4 +24,3 @@ export * from './src/styles/core-styles';
 export * from './src/undefined';
 export * from './src/media/invert-media-query';
 export * from './src/style-utils';
-export * from './src/theme/dynamic-styles';
