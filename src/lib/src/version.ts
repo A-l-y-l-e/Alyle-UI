976b2f82cef84ecfd99b9b63bf6d5ec64a10848b
alyleui <alyleui@gmail.com>
2018-08-10 19:02:45 -0500

    bump version 1.7.0-beta.4iic8

--- src/lib/src/version.ts
+++ src/lib/src/version.ts
@@ -1,2 +1,2 @@
-export const AUI_VERSION = '1.7.0-beta.4ih8i';
-export const AUI_LAST_UPDATE = '2018-08-10T23:36:05.786Z';
+export const AUI_VERSION = '1.7.0-beta.4iic8';
+export const AUI_LAST_UPDATE = '2018-08-10T23:59:56.290Z';
