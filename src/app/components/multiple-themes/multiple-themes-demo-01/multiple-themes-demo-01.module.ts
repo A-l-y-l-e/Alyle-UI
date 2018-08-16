ce0812c46ca8700887783c5565807a5b8a91154c
alyleui <alyleui@gmail.com>
2018-08-15 22:55:30 -0500

    chore: update docs multiple themes

--- src/app/components/multiple-themes/multiple-themes-demo-01/multiple-themes-demo-01.module.ts
+++ src/app/components/multiple-themes/multiple-themes-demo-01/multiple-themes-demo-01.module.ts
@@ -4,14 +4,14 @@ import { MultipleThemesDemo01Component } from './multiple-themes-demo-01.compone
 import { LyCommonModule } from '@alyle/ui';
 import { LyButtonModule } from '@alyle/ui/button';
 import { RouterModule } from '@angular/router';
-import { MinimaThemeModule } from '@alyle/ui/themes/minima';
+import { ThemeMinimaModule } from '@alyle/ui/themes/minima';
 import { LyCardModule } from '@alyle/ui/card';
 
 @NgModule({
   imports: [
     CommonModule,
     LyCommonModule,
-    MinimaThemeModule, // <-- import theme module
+    ThemeMinimaModule, // <-- import theme module
     RouterModule,
     LyCommonModule,
     LyCardModule,
