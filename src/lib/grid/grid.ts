b9a331d895540e5fa59ab901ed554a5b641e84dc
alyleui <alyleui@gmail.com>
2018-08-29 21:56:57 -0500

    feat(grid): add `direction` & `justify`

--- src/lib/grid/grid.ts
+++ src/lib/grid/grid.ts
@@ -4,6 +4,17 @@ import { LY_MEDIA_QUERIES } from '@alyle/ui/responsive';
 
 const COL_VALUES = { };
 
+const ALIGN_ALIAS = {
+  rowReverse: 'row-reverse',
+  columnReverse: 'column-reverse',
+  wrapReverse: 'wrap-reverse',
+  start: 'flex-start',
+  end: 'flex-end',
+  between: 'space-between',
+  around: 'space-around',
+  evenly: 'space-evenly',
+};
+
 const styles = ({
   root: {
     width: '100%',
@@ -13,7 +24,11 @@ const styles = ({
   }
 });
 
+type Justify = 'start' | 'center' | 'end' | 'between' | 'around' | 'evenly';
+type Direction = 'row' | 'row-reverse' | 'column' | 'column-reverse';
+
 /**
+ * Grid container
  * example:
  * <ly-grid container [spacing]="'16 8@XSmall'">
  *   <ly-grid item [col]="'6 12@XSmall'">
@@ -33,9 +48,17 @@ export class LyGrid {
    * @ignore
    */
   classes = this.theme.addStyleSheet(styles, 'lyGrid');
+
   private _spacing: string | number;
   private _spacingClass: string;
+
   private _negativeMarginClass: string;
+
+  private _justify: Justify;
+  private _justifyClass: string;
+
+  private _direction: Direction;
+  private _directionClass: string;
 
   /**
    * Defines the space between the component with the `item` attribute.
@@ -91,7 +114,7 @@ export class LyGrid {
           });
           return negativeMarginStyles as any;
         }
-      }, this.elementRef.nativeElement, this._negativeMarginClass);
+      }, this.el.nativeElement, this._negativeMarginClass);
     }
   }
 
@@ -99,12 +122,74 @@ export class LyGrid {
     return this._spacingClass;
   }
 
+  /** Defines the justify-content style property. */
+  @Input()
+  get justify(): Justify {
+    return this._justify;
+  }
+  set justify(val: Justify) {
+    if (val !== this.justify) {
+      this._justifyClass = this.theme.addStyle(`lyGrid-justify:${val}`, () => {
+        let justifyStyles: {
+          justifyContent?: string
+        };
+        eachMedia(val, (value, media, isMedia) => {
+          const newJustifyStyles = {
+            justifyContent: value in ALIGN_ALIAS
+            ? ALIGN_ALIAS[value]
+            : value
+          };
+          if (isMedia) {
+            if (!justifyStyles) {
+              justifyStyles = {};
+            }
+            justifyStyles[`@media ${this.mediaQueries[media]}`] = newJustifyStyles;
+          } else {
+            justifyStyles = newJustifyStyles;
+          }
+        });
+        return justifyStyles as any;
+      }, this.el.nativeElement, this._justifyClass);
+    }
+  }
+
+  /** Defines the justify-content style property. */
+  @Input()
+  get direction(): Direction {
+    return this._direction;
+  }
+  set direction(val: Direction) {
+    if (val !== this.direction) {
+      this._directionClass = this.theme.addStyle(`lyGrid-direction:${val}`, () => {
+        let directionStyles: {
+          flexDirection?: string
+        };
+        eachMedia(val, (value, media, isMedia) => {
+          const newDirectionStyles = {
+            flexDirection: value in ALIGN_ALIAS
+            ? ALIGN_ALIAS[value]
+            : value
+          };
+          if (isMedia) {
+            if (!directionStyles) {
+              directionStyles = {};
+            }
+            directionStyles[`@media ${this.mediaQueries[media]}`] = newDirectionStyles;
+          } else {
+            directionStyles = newDirectionStyles;
+          }
+        });
+        return directionStyles as any;
+      }, this.el.nativeElement, this._directionClass);
+    }
+  }
+
   constructor(
     @Optional() @Inject(LY_MEDIA_QUERIES) private mediaQueries: any,
     private theme: LyTheme2,
-    private elementRef: ElementRef,
+    private el: ElementRef,
   ) {
-    this.elementRef.nativeElement.classList.add(this.classes.root);
+    this.el.nativeElement.classList.add(this.classes.root);
   }
 }
 
@@ -122,7 +207,7 @@ export class LyGridCol implements OnInit {
   }
   set col(val: string | number) {
     if (val !== this.col) {
-      this._colClass = this.theme.addStyle(`lyGrid-negative-margin:${val}`, () => {
+      this._colClass = this.theme.addStyle(`lyGrid-col:${val}`, () => {
         if (typeof val === 'number') {
           return getColStyle(val);
         } else {
@@ -155,7 +240,7 @@ export class LyGridCol implements OnInit {
     private theme: LyTheme2
   ) {
     if (!gridContainer) {
-      throw new Error(`Rquire`);
+      throw new Error(`Require parent grid`);
     }
   }
 
