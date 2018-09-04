e0dd1d6f414af363b5f9d731463a83500d019dce
alyleui <alyleui@gmail.com>
2018-09-04 00:16:11 -0500

    docs: update demos

--- src/app/components/menu-demo/menu-demo.module.ts
+++ src/app/components/menu-demo/menu-demo.module.ts
@@ -7,12 +7,14 @@ import { MenuDemoComponent } from './menu-demo/menu-demo.component';
 import { MenuExample01Module } from './menu-example-01/menu-example-01.module';
 import { MenuDemo01Module } from './menu-demo-01/menu-demo-01.module';
 import { MenuDemo02Module } from './menu-demo-02/menu-demo-02.module';
+import { LyCommonModule } from '@alyle/ui';
 
 @NgModule({
   imports: [
     CommonModule,
     MenuDemoRoutingModule,
     DemoViewModule,
+    LyCommonModule,
     MenuExample01Module,
     MenuDemo01Module,
     MenuDemo02Module
