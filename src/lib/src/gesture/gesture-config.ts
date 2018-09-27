38d6dfaa1a3d2002d9ae6caec904c12e27649871
alyleui <alyleui@gmail.com>
2018-09-27 15:12:02 -0500

    fix: fix gesture config

--- src/lib/src/gesture/gesture-config.ts
+++ src/lib/src/gesture/gesture-config.ts
@@ -4,40 +4,30 @@ import { HammerOptions, HammerInstance } from './gesture-annotations';
 
 export const LY_HAMMER_OPTIONS = new InjectionToken<HammerOptions>('LY_HAMMER_OPTIONS');
 
+const HAMMER_GESTURES_EVENTS = [
+  'slide',
+  'slidestart',
+  'slideend',
+  'slideright',
+  'slideleft'
+];
+
 @Injectable()
 export class LyHammerGestureConfig extends HammerGestureConfig {
-  private _hammer = typeof window !== 'undefined' ? (window as any).Hammer : null;
-  events: string[] = this._hammer ? [
-    'slide',
-    'slidestart',
-    'slideend',
-    'slideright',
-    'slideleft'
-  ] : [];
+  events: string[] = HAMMER_GESTURES_EVENTS;
   constructor(
-    @Optional() @Inject(LY_HAMMER_OPTIONS) private _hammerOptions: HammerOptions,
-    // private coreTheme: CoreTheme,
+    @Optional() @Inject(LY_HAMMER_OPTIONS) private _hammerOptions: HammerOptions
   ) {
     super();
   }
   buildHammer(element: HTMLElement): HammerInstance {
-    // if (Platform.isBrowser) {
-    //   const newClass = this.coreTheme.setUpStyle('k-hammer-css', {
-    //     '': () => (
-    //       `user-select: none;` +
-    //       `-webkit-user-drag: none;` +
-    //       `-webkit-tap-highlight-color: rgba(0, 0, 0, 0);`
-    //     )
-    //   });
-    //   element.classList.add(newClass);
-    // }
-    const mc = new this._hammer(element, this._hammerOptions || undefined);
+    const hammer = typeof window !== 'undefined' ? (window as any).Hammer : null;
+    const mc = new hammer(element, this._hammerOptions || undefined);
 
-    const pan = new this._hammer.Pan();
-    const swipe = new this._hammer.Swipe();
+    const pan = new hammer.Pan();
+    const swipe = new hammer.Swipe();
     const slide = this._createRecognizer(pan, {event: 'slide', threshold: 0}, swipe);
 
-    slide.recognizeWith(swipe);
     pan.recognizeWith(swipe);
 
     // Add customized gestures to Hammer manager
