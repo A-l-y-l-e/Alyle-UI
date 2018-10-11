c0bc52ba0e0a64aa4e5be2aa2970441b9ad7f7c7
alyleui <alyleui@gmail.com>
2018-10-10 19:04:01 -0500

    remove unused variables

--- src/lib/field/label.ts
+++ src/lib/field/label.ts
@@ -1,10 +1,6 @@
-import { Directive, TemplateRef, ViewContainerRef, ViewRef } from '@angular/core';
+import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'ly-field > ly-label'
 })
-export class LyLabel {
-  constructor(
-    public _vcr: ViewContainerRef
-  ) { }
-}
+export class LyLabel { }
