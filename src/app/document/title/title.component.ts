6c9658fe1df3ab138145f0efab01a5e996fcbbaa
alyleui <alyleui@gmail.com>
2018-10-16 23:51:20 -0500

    docs: update api

--- src/app/document/title/title.component.ts
+++ src/app/document/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Platform } from '@alyle/ui';
 import { environment } from '@env/environment';
@@ -6,8 +6,8 @@ import { environment } from '@env/environment';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class TitleComponent implements OnInit {
   private _route: string;
@@ -21,9 +21,16 @@ export class TitleComponent implements OnInit {
     const varArray = val.split('/').filter(_ => !!_);
     const latestItem = varArray[varArray.length - 1];
     this.urls = varArray.map(_ => _.charAt(0).toUpperCase() + _.slice(1));
-    this.title = toTitle(latestItem === 'api' ? `${varArray[varArray.length - 2]} API` : latestItem);
+    this.title = toTitle(latestItem);
     if (this.title) {
-      this.titleService.setTitle(`${this.title} | ${this.defaultTitle}`);
+      if (varArray.some(_ => _ === 'layout' || _ === 'components')) {
+        const name = varArray[0] === 'components' ? varArray[0].slice(0, -1) : varArray[0];
+        this.titleService.setTitle(`${this.title} Angular ${name} | ${this.defaultTitle}`);
+      } else if (varArray.some(_ => _ === 'api')) {
+        this.titleService.setTitle(`${this.title} API | ${this.defaultTitle}`);
+      } else {
+        this.titleService.setTitle(`${this.title} | ${this.defaultTitle}`);
+      }
     } else {
       this.titleService.setTitle(this.defaultTitle);
     }
