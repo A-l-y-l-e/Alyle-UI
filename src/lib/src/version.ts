1f3a05f3435dd60bdda5a8ff71cb126713a5eaaa
alyleui <alyleui@gmail.com>
2018-10-25 19:23:11 +0000

    bump version 1.7.5
    [ci skip]

--- src/lib/src/version.ts
+++ src/lib/src/version.ts
@@ -1,2 +1,2 @@
-export const AUI_VERSION = '1.7.4';
-export const AUI_LAST_UPDATE = '2018-10-25T19:04:22.582Z';
+export const AUI_VERSION = '1.7.5';
+export const AUI_LAST_UPDATE = '2018-10-25T19:19:03.220Z';
