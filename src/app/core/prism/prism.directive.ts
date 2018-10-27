f838f0f41d425edf899a8a2c63db12ddd907eefd
alyleui <alyleui@gmail.com>
2018-10-27 17:44:28 -0500

    docs: update theme for prism

--- src/app/core/prism/prism.directive.ts
+++ src/app/core/prism/prism.directive.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, Input, OnInit, Renderer2, ChangeDetectionStrategy } from '@angular/core';
-import { Platform, LyTheme2 } from '@alyle/ui';
+import { Platform, LyTheme2, ThemeVariables } from '@alyle/ui';
+import { PrismService } from './prism.service';
 
 const Prism = require('prismjs');
 const PrismTypescript = require('prismjs/components/prism-typescript');
@@ -8,12 +9,127 @@ const PrismBash = require('prismjs/components/prism-bash');
 const PrismJson = require('prismjs/components/prism-json');
 
 const classes = theme => ({
+  '@global': {
+    'prism': {
+      display: 'block',
+      padding: '1em'
+    }
+  },
   root: {
     color: theme.codeColor,
     backgroundColor: theme.codeBg,
-    margin: '1em 0'
+    margin: '1em 0',
+    'pre[class*="language-"]': {
+      padding: '1em',
+      margin: '.5em 0',
+      overflow: 'auto'
+    },
+    '& code[class*="language-"], & pre[class*="language-"]': {
+      background: 'none',
+      fontFamily: `'Fira Code', Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace`,
+      textAlign: 'left',
+      fontSize: '13px',
+      fontWeight: 500,
+      whiteSpace: 'pre-wrap',
+      wordSpacing: 'normal',
+      wordBreak: 'break-word',
+      wordWrap: 'normal',
+      lineHeight: 1.5,
+      '-moz-tab-size': 4,
+      '-o-tab-size': 4,
+      tabSize: 4,
+      '-webkit-hyphens': 'none',
+      '-moz-hyphens': 'none',
+      '-ms-hyphens': 'none',
+      hyphens: 'none',
+    }
   }
 });
+
+const STYLES = (theme: ThemeVariables) => ({
+  token: {
+    '&{keyword}, &{selector-tag}, &{title}, &{section}, &{doctag}, &{name}, &{strong}': {
+      color: theme.accent.default
+    },
+    '&{comment}': {
+      color: 'rgba(115, 129, 145, 0.65)'
+    },
+    '&{string}, &{title}, &{section}, &{built_in}, &{literal}, &{type}, &{addition}, &{tag}, &{quote}, &{name}, &{selector-id}, &{selector-class}': {
+      color: '#ff5085'
+    },
+    '&{meta}, &{subst}, &{symbol}, &{regexp}, &{attribute}, &{deletion}, &{variable}, &{template-variable}, &{link}, &{bullet}': {
+      color: '#4c81c9'
+    },
+    '&{emphasis}': {
+      fontStyle: 'italic'
+    },
+    '&{function}': {
+      color: '#4584ff'
+    },
+    '&{tag} > {punctuation}, &{attr-value} > {punctuation}:first-child': {
+      color: '#39ADB5'
+    },
+    '&{attr-name}': {
+      color: '#FFB62C'
+    },
+    '&{attr-value}': {
+      color: '#7c4dff'
+    },
+    '&{string}': {
+      color: '#8BC34A'
+    },
+    '&{number}': {
+      color: 'rgb(36, 212, 158)'
+    },
+    '&{punctuation}, &{operator}': {
+      color: '#7c4dff'
+    },
+    '&{class-name}': {
+      color: theme.primary.default
+    },
+    '&{constant}': {
+      color: '#EF5350'
+    }
+  },
+  keyword: { },
+  'selector-tag': { },
+  title: { },
+  section: { },
+  doctag: { },
+  name: { },
+  strong: { },
+  comment: { },
+  string: { },
+  built_in: { },
+  literal: { },
+  type: { },
+  addition: { },
+  tag: { },
+  quote: { },
+  'selector-id': { },
+  'selector-class': { },
+  meta: { },
+  subst: { },
+  symbol: { },
+  regexp: { },
+  attribute: { },
+  deletion: { },
+  variable: { },
+  'template-variable': { },
+  link: { },
+  bullet: { },
+  emphasis: { },
+  function: { },
+  punctuation: { },
+  number: { },
+  operator: { },
+  constant: { },
+  'attr-value': { },
+  'attr-name': { },
+  'class-name': { }
+});
+
+const NOP_STYLES = {};
 
 @Component({
   selector: '[prism], prism',
@@ -21,13 +137,10 @@ const classes = theme => ({
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PrismDirective implements OnInit {
-  classes = this.theme.addStyleSheet(classes, 'prism', -1);
+  classes = this.theme.addStyleSheet(classes, -1);
+  prismClasses = Platform.isBrowser ? this.theme.addStyleSheet(STYLES, -1) : NOP_STYLES;
   private _code: string;
   @Input() language = 'ts';
-  @Input()
-  set bg(val) {
-    console.log('remove this.', {val});
-  }
   @Input()
   set code(val) {
     this._code = val;
@@ -39,7 +152,10 @@ export class PrismDirective implements OnInit {
     private theme: LyTheme2,
     private _elementRef: ElementRef,
     private renderer: Renderer2,
-  ) { }
+    _prismService: PrismService
+  ) {
+    _prismService.setCustomClass(this.prismClasses);
+  }
 
   ngOnInit() {
     this.renderer.addClass(this._elementRef.nativeElement, this.classes.root);
