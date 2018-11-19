6e6e59834e4e8cf4bfb53ad12daa56535bb3f84a
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-11-19 02:30:08 -0500

    export all common behaviors

--- src/lib/src/common/index.ts
+++ src/lib/src/common/index.ts
@@ -1,4 +1,10 @@
+export { buildCommonBehaviors } from './build-common-behaviors';
 export { mixinDisableRipple } from './disable-ripple';
 export { mixinDisabled } from './disabled';
 export { mixinColor } from './color';
 export { mixinBg } from './bg';
+export { mixinFlat } from './flat';
+export { mixinRaised } from './raised';
+export { mixinOutlined } from './outlined';
+export { mixinElevation } from './elevation';
+export { mixinShadowColor } from './shadow-color';
