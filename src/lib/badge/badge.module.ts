1291b460277926562d3217f0d6eef2f0da90187a
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-11-19 23:05:55 -0500

    badge: update

--- src/lib/badge/badge.module.ts
+++ src/lib/badge/badge.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { LyCommonModule } from '@alyle/ui';
 import { LyBadge } from './badge';
 
 @NgModule({
-  imports: [CommonModule, LyCommonModule],
   exports: [LyBadge, LyCommonModule],
   declarations: [LyBadge]
 })
