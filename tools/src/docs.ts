5864ffa32863447cce863c473a33b25c43ba7676
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-11-23 02:55:06 -0500

    docs(): add `APIListLarge`

--- tools/src/docs.ts
+++ tools/src/docs.ts
@@ -8,41 +8,93 @@ import { join } from 'path';
 import { ProjectReflection } from 'typedoc';
 
 interface DocsPackage {
-  name: string;
   children: {
     name: string
     type: string
+  }[];
+}
+interface DocsPackageLarge {
+  directiveList: {
+    name: string
+    selector: string
+    exportAs: string
+    inputs: string
+  }[];
+  componentList: {
+    name: string
+    selector: string
+    inputs: string
+    exportAs: string
+  }[];
+  [type: string]: {
+    name: string
   }[];
 }
 
 const docsJSON: ProjectReflection = JSON.parse(fs.readFileSync(join(process.cwd(), 'dist/docs.json')).toString());
 
 const APIList: {[name: string]: DocsPackage} = {};
+const APIListLarge: {[name: string]: DocsPackageLarge} = {};
 
 docsJSON.children.forEach(child => {
   if (child.children) {
-    const name = getPackageName(child.name);
+    const pkgName = getPackageName(child.name);
 
     /**
      * if not exist add defaults
      */
-    if (!APIList[name]) {
-      APIList[name] = {
-        name,
+    if (!APIList[pkgName]) {
+      APIList[pkgName] = {
         children: []
       };
     }
+
+    if (!APIListLarge[pkgName]) {
+      APIListLarge[pkgName] = {
+        classeList: [],
+        constList: [],
+        directiveList: [],
+        enumList: [],
+        functionList: [],
+        interfaceList: [],
+        ngModuleList: [],
+        TypeAliasList: [],
+        componentList: []
+      };
+    }
+
     child.children.forEach(_child => {
-      APIList[name].children.push({
-        name: _child.name,
-        type: _child.kindString
+      // igonre styles
+      if (_child.name.toLowerCase() === 'styles') {
+        return;
+      }
+      const { name, decorators, kindString } = _child;
+      const type = decorators ? decorators[0].name : kindString;
+      APIList[pkgName].children.push({
+        name,
+        type
       });
+      if (type === 'Component') {
+        console.log(decorators[0].arguments.obj);
+        const selector = ((/selector:\s?\'?\`?([\w\-\,\[\]\s]+)\'/g).exec(decorators[0].arguments.obj))[1];
+        const exportAs = ((/exportAs:\s?\'?\`?([\w\-\,\[\]\s]+)\'/g).exec(decorators[0].arguments.obj) || [])[1] || null;
+        APIListLarge[pkgName].componentList.push({
+          name,
+          selector,
+          inputs: null,
+          exportAs
+        });
+      }
     });
-    console.log(name);
+    console.log(pkgName);
   }
 });
 
-console.log(JSON.stringify(APIList, undefined, 2));
+// console.log(JSON.stringify(APIList, undefined, 2));
+console.log(JSON.stringify(APIListLarge, undefined, 2));
+
+fs.writeFileSync(join(process.cwd(), 'docs/APIList.json'), JSON.stringify(APIList, undefined, 2), 'utf8');
+fs.writeFileSync(join(process.cwd(), 'docs/APIList.min.json'), JSON.stringify(APIList), 'utf8');
 
 function getPackageName(name: string) {
   // ignore src
