d392eb61763129866e9cc5ad4c172aff3e6e4997
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-11-24 03:02:40 -0500

    styles

--- src/lib/tooltip/tooltip.ts
+++ src/lib/tooltip/tooltip.ts
@@ -1,9 +1,20 @@
 import { Directive } from '@angular/core';
+import { LyTheme2, LY_COMMON_STYLES } from '@alyle/ui';
+
+const STYLE_PRIORITY = -2;
+const styles = ({
+  root: {
+    ...LY_COMMON_STYLES.fill
+  }
+});
 
 @Directive({
   selector: '[lyTooltip]',
   exportAs: 'lyTooltip'
 })
 export class LyTooltip {
-
+  readonly classes = this._theme.addStyleSheet(styles, STYLE_PRIORITY);
+  constructor(
+    private _theme: LyTheme2
+  ) { }
 }
