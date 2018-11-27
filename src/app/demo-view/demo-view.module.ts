b44fed275786588e4dc5cee3293eb2867dc9aa4f
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-11-27 17:32:05 -0500

    docs: add tooltip

--- src/app/demo-view/demo-view.module.ts
+++ src/app/demo-view/demo-view.module.ts
@@ -6,11 +6,12 @@ import { LyTabsModule } from '@alyle/ui/tabs';
 import { LyToolbarModule } from '@alyle/ui/toolbar';
 import { LyButtonModule } from '@alyle/ui/button';
 import { LyIconModule } from '@alyle/ui/icon';
+import { LyCardModule } from '@alyle/ui/card';
+import { LyTooltipModule } from '@alyle/ui/tooltip';
 
 import { PrismModule } from '../core/prism/prism.module';
 
 import { ViewComponent } from './view/view.component';
-import { LyCardModule } from '@alyle/ui/card';
 
 @NgModule({
   imports: [
@@ -22,6 +23,7 @@ import { LyCardModule } from '@alyle/ui/card';
     LyIconModule,
     LyCommonModule,
     LyCardModule,
+    LyTooltipModule,
     RouterModule
   ],
   exports: [ViewComponent],
