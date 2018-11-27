0a3db67c8f83a50584243892d1e21983cbf625e5
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-11-27 17:25:55 -0500

    fix(tooltip): fix `No component factory found for LyOverlayBackdrop`

--- src/lib/tooltip/tooltip.module.ts
+++ src/lib/tooltip/tooltip.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { LyTooltip } from './tooltip';
+import { LyOverlayModule } from '@alyle/ui';
 
 @NgModule({
+  imports: [LyOverlayModule],
   declarations: [LyTooltip],
   exports: [LyTooltip]
 })
