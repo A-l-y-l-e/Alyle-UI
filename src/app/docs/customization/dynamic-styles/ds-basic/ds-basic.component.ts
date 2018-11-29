ab5bf6b64b651911ec43a4582f8b8d68806bb465
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-11-29 03:33:18 -0500

    update docs

--- src/app/docs/customization/dynamic-styles/ds-basic/ds-basic.component.ts
+++ src/app/docs/customization/dynamic-styles/ds-basic/ds-basic.component.ts
@@ -3,14 +3,17 @@ import { LyTheme2, ThemeVariables } from '@alyle/ui';
 
 /**
  * Basic style
- * @param theme Theme config
+ * @param theme ThemeVariables
  */
 const styles = (theme: ThemeVariables) => ({
+  // Style name, is optional, this is used to add a prefix to all classes,
+  // it will only be seen in dev mode
+  $name: 'example',
   demo: {                         // this would be like the name of the class
     color: theme.primary.default, // style
     borderStart: '2px solid',     // support for rtl & ltr
     paddingStart: '.5em',         // support for rtl & ltr
-    '&:hover': {                  // `&` is equal to `root` and therefore it would be 'demo:hover'
+    '&:hover': {                  // `&` is equal to `demo` and therefore it would be 'demo:hover'
       color: theme.accent.default // style
     }
   },
--- src/app/docs/customization/dynamic-styles/dynamic-styles.component.ts
+++ src/app/docs/customization/dynamic-styles/dynamic-styles.component.ts
@@ -7,11 +7,14 @@ import { Component } from '@angular/core';
 export class DynamicStylesComponent {
   codeBasicStyle = `
 const styles = (theme: ThemeVariables) => ({
+  // Style name, is optional, this is used to add a prefix to all classes,
+  // it will only be seen in dev mode
+  $name: 'example',
   demo: {                         // this would be like the name of the class
     color: theme.primary.default, // style
     borderStart: '2px solid',     // support for rtl & ltr
     paddingStart: '.5em',         // support for rtl & ltr
-    '&:hover': {                  // \`&\` is equal to \`root\` and therefore it would be 'demo:hover'
+    '&:hover': {                  // \`&\` is equal to \`demo\` and therefore it would be 'demo:hover'
       color: theme.accent.default // style
     }
   },
