79598d1fc87c1fdd414e0147d6494860a8e6040e
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-12-03 03:28:15 -0500

    update docs

--- src/app/docs/components/avatar-demo/basic-uses-avatar/basic-uses-avatar.module.ts
+++ src/app/docs/components/avatar-demo/basic-uses-avatar/basic-uses-avatar.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicUsesAvatarComponent } from './basic-uses-avatar.component';
 import { LyAvatarModule } from '@alyle/ui/avatar';
+import { LyIconModule } from '@alyle/ui/icon';
+import { LyGridModule } from '@alyle/ui/grid';
 
 @NgModule({
   imports: [
     CommonModule,
-    LyAvatarModule
+    LyAvatarModule,
+    LyIconModule,
+    LyGridModule
   ],
   exports: [BasicUsesAvatarComponent],
   declarations: [BasicUsesAvatarComponent]
