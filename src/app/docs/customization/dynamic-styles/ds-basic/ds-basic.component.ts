5ed843ee2a3742abd0ba6c4368abb221955667f1
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-12-07 10:49:08 -0500

    update docs

--- src/app/docs/customization/dynamic-styles/ds-basic/ds-basic.component.ts
+++ src/app/docs/customization/dynamic-styles/ds-basic/ds-basic.component.ts
@@ -3,18 +3,19 @@ import { LyTheme2, ThemeVariables } from '@alyle/ui';
 
 /**
  * Basic style
- * @param theme ThemeVariables
  */
 const styles = (theme: ThemeVariables) => ({
   // Style name, is optional, this is used to add a prefix to all classes,
   // it will only be seen in dev mode
   $name: 'example',
-  demo: {                         // this would be like the name of the class
-    color: theme.primary.default, // style
-    borderBefore: '2px solid',     // support for rtl & ltr
-    paddingBefore: '.5em',         // support for rtl & ltr
+  // this would be like the name of the class
+  demo: {
+    color: theme.primary.default,
+    // support for direction rtl/ltr
+    borderBefore: '2px solid',    // css-ltr: border-left, css-rtl: border-right
+    paddingBefore: '.5em',        // css-ltr: padding-left, css-rtl: padding-right
     '&:hover': {                  // `&` is equal to `demo` and therefore it would be 'demo:hover'
-      color: theme.accent.default // style
+      color: theme.accent.default
     }
   },
   buttonLink: {
