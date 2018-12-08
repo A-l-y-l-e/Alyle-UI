e01eae670f704484e713bf574e98e57420563192
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-12-08 18:24:09 -0500

    feat(core): add `AlignAlias`

---
+++ src/lib/src/position/align.ts
@@ -0,0 +1,10 @@
+export const enum AlignAlias {
+  rowReverse = 'row-reverse',
+  columnReverse = 'column-reverse',
+  wrapReverse = 'wrap-reverse',
+  start = 'flex-start',
+  end = 'flex-end',
+  between = 'space-between',
+  around = 'space-around',
+  evenly = 'space-evenly'
+}
