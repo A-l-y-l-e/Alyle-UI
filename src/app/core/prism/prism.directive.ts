dca0edc61b9aead3b44211e30559f09d75a26d14
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-12-19 15:08:19 -0500

    update docs

--- src/app/core/prism/prism.directive.ts
+++ src/app/core/prism/prism.directive.ts
@@ -16,7 +16,6 @@ Prism.languages.typescript = Prism.languages.extend('javascript', {
 Prism.languages.ts = Prism.languages.typescript;
 
 require('prismjs/components/prism-markdown');
-require('prismjs/components/prism-bash');
 require('prismjs/components/prism-json');
 Prism.hooks.add('wrap', function(env) {
   if (env.type === 'string') {
@@ -80,7 +79,8 @@ const GLOBAL_STYLES = {
     },
     'prism': {
       display: 'block',
-      padding: '1em'
+      padding: '1em',
+      overflow: 'hidden'
     }
   }
 };
@@ -102,7 +102,7 @@ const classes = (theme: AUIThemeVariables) => ({
       textAlign: 'left',
       fontSize: '13px',
       fontWeight: 500,
-      whiteSpace: 'pre-wrap',
+      whiteSpace: 'pre',
       wordSpacing: 'normal',
       wordBreak: 'break-word',
       wordWrap: 'normal',
@@ -256,3 +256,90 @@ export class PrismDirective implements OnInit {
   }
 
 }
+
+// tslint:disable-next-line:no-shadowed-variable
+(function(Prism) {
+  const insideString = {
+    variable: [
+      // Arithmetic Environment
+      {
+        pattern: /\$?\(\([\s\S]+?\)\)/,
+        inside: {
+          // If there is a $ sign at the beginning highlight $(( and )) as variable
+          variable: [{
+              pattern: /(^\$\(\([\s\S]+)\)\)/,
+              lookbehind: true
+            },
+            /^\$\(\(/
+          ],
+          number: /\b0x[\dA-Fa-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:[Ee]-?\d+)?/,
+          // Operators according to https://www.gnu.org/software/bash/manual/bashref.html#Shell-Arithmetic
+          operator: /--?|-=|\+\+?|\+=|!=?|~|\*\*?|\*=|\/=?|%=?|<<=?|>>=?|<=?|>=?|==?|&&?|&=|\^=?|\|\|?|\|=|\?|:/,
+          // If there is no $ sign at the beginning highlight (( and )) as punctuation
+          punctuation: /\(\(?|\)\)?|,|;/
+        }
+      },
+      // Command Substitution
+      {
+        pattern: /\$\([^)]+\)|`[^`]+`/,
+        greedy: true,
+        inside: {
+          variable: /^\$\(|^`|\)$|`$/
+        }
+      },
+      /\$(?:[\w#?*!@]+|\{[^}]+\})/i
+    ]
+  };
+
+  Prism.languages.bash = {
+    'shebang': {
+      pattern: /^#!\s*\/bin\/bash|^#!\s*\/bin\/sh/,
+      alias: 'important'
+    },
+    'comment': {
+      pattern: /(^|[^"{\\])#.*/,
+      lookbehind: true
+    },
+    'string': [
+      // Support for Here-Documents https://en.wikipedia.org/wiki/Here_document
+      {
+        pattern: /((?:^|[^<])<<\s*)["']?(\w+?)["']?\s*\r?\n(?:[\s\S])*?\r?\n\2/,
+        lookbehind: true,
+        greedy: true,
+        inside: insideString
+      },
+      {
+        pattern: /(["'])(?:\\[\s\S]|\$\([^)]+\)|`[^`]+`|(?!\1)[^\\])*\1/,
+        greedy: true,
+        inside: insideString
+      }
+    ],
+    'variable': insideString.variable,
+    // Originally based on http://ss64.com/bash/
+    'function': {
+      // tslint:disable-next-line:max-line-length
+      pattern: /(^|[\s;|&])(?:alias|apropos|apt-get|aptitude|aspell|awk|basename|bash|bc|bg|builtin|bzip2|cal|cat|cd|cfdisk|chgrp|chmod|chown|chroot|chkconfig|cksum|clear|cmp|comm|command|cp|cron|crontab|csplit|curl|cut|date|dc|dd|ddrescue|df|diff|diff3|dig|dir|dircolors|dirname|dirs|dmesg|du|egrep|eject|enable|env|ethtool|eval|exec|expand|expect|export|expr|fdformat|fdisk|fg|fgrep|file|find|fmt|fold|format|free|fsck|ftp|fuser|gawk|getopts|git|grep|groupadd|groupdel|groupmod|groups|gzip|hash|head|help|hg|history|hostname|htop|iconv|id|ifconfig|ifdown|ifup|import|install|jobs|join|kill|killall|less|link|ln|locate|logname|logout|look|lpc|lpr|lprint|lprintd|lprintq|lprm|ls|lsof|make|man|mkdir|mkfifo|mkisofs|mknod|more|most|mount|mtools|mtr|mv|mmv|nano|netstat|nice|nl|nohup|notify-send|npm|yarn|nslookup|open|op|passwd|paste|pathchk|ping|pkill|popd|pr|printcap|printenv|printf|ps|pushd|pv|pwd|quota|quotacheck|quotactl|ram|rar|rcp|read|readarray|readonly|reboot|rename|renice|remsync|rev|rm|rmdir|rsync|screen|scp|sdiff|sed|seq|service|sftp|shift|shopt|shutdown|sleep|slocate|sort|source|split|ssh|stat|strace|su|sudo|sum|suspend|sync|tail|tar|tee|test|time|timeout|times|touch|top|traceroute|trap|tr|tsort|tty|type|ulimit|umask|umount|unalias|uname|unexpand|uniq|units|unrar|unshar|uptime|useradd|userdel|usermod|users|uuencode|uudecode|v|vdir|vi|vmstat|wait|watch|wc|wget|whereis|which|who|whoami|write|xargs|xdg-open|yes|zip)(?=$|[\s;|&])/,
+      lookbehind: true
+    },
+    'keyword': {
+      pattern: /(^|[\s;|&])(?:let|:|\.|if|then|else|elif|fi|for|break|continue|while|in|case|function|select|do|done|until|echo|exit|return|set|declare)(?=$|[\s;|&])/,
+      lookbehind: true
+    },
+    'boolean': {
+      pattern: /(^|[\s;|&])(?:true|false)(?=$|[\s;|&])/,
+      lookbehind: true
+    },
+    'operator': /&&?|\|\|?|==?|!=?|<<<?|>>|<=?|>=?|=~/,
+    'punctuation': /\$?\(\(?|\)\)?|\.\.|[{}[\];]/
+  };
+
+  const inside = (insideString.variable[1] as any).inside;
+  inside.string = Prism.languages.bash.string;
+  inside['function'] = Prism.languages.bash['function'];
+  inside.keyword = Prism.languages.bash.keyword;
+  inside['boolean'] = Prism.languages.bash['boolean'];
+  inside.operator = Prism.languages.bash.operator;
+  inside.punctuation = Prism.languages.bash.punctuation;
+
+  Prism.languages.shell = Prism.languages.bash;
+})(Prism);
