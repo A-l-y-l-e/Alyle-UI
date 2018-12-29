927e5456fb1017030a145ca6cc09fdf2e9de9bae
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-12-28 23:26:58 -0500

    update docs

--- src/app/docs/components/field-demo/field-playground/field-playground.component.ts
+++ src/app/docs/components/field-demo/field-playground/field-playground.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'aui-field-playground',
-  templateUrl: './field-playground.component.html'
+  templateUrl: './field-playground.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FieldPlaygroundComponent {
   hide = true;
   appearance = new FormControl();
   isReadonly = new FormControl();
   isDisabled = new FormControl();
-  password = new FormControl('pass');
+  password = new FormControl('', Validators.required);
 }
