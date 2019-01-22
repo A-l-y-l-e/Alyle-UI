cf12bc512efeaf9474cad57c2b13686fcf0375f9
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2019-01-22 00:13:03 +0000

    release @alyle/ui 2.0.5 :tada:
    [ci skip]

--- src/lib/src/version.ts
+++ src/lib/src/version.ts
@@ -1,2 +1,2 @@
-export const AUI_VERSION = '2.0.4';
-export const AUI_LAST_UPDATE = '2019-01-15T00:13:40.259Z';
+export const AUI_VERSION = '2.0.5';
+export const AUI_LAST_UPDATE = '2019-01-22T00:09:03.558Z';
