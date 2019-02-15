678d6dc0609c7a1f2349ff3ab683e7ab8970b70e
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2019-02-14 19:17:45 -0500

    ­

--- src/lib/avatar/avatar.ts
+++ src/lib/avatar/avatar.ts
@@ -30,7 +30,9 @@ const STYLES = (theme: ThemeVariables) => ({
       width: '100%',
       height: '100%',
       borderRadius: '50%',
-      display: 'block'
+      display: 'block',
+      objectFit: 'cover',
+      '-webkit-background-clip': 'padding-box'
     }
   }
 });
