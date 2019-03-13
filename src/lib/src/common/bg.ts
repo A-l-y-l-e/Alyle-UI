62845adfe76ddb0b38609f6cf078d4e0247d3f95
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2019-03-13 00:56:37 -0400

    fix(core): avoid conflicts with common behaviors.

--- src/lib/src/common/bg.ts
+++ src/lib/src/common/bg.ts
@@ -4,17 +4,21 @@ const DEFAULT_BG = 'primary';
 
 export interface CanBg {
   bg: string;
+  /**
+   * It is only used for common behavior, therefore, it should not be used for other purposes.
+   */
+  readonly _superHyperInternalPropertyBg: string;
 }
 
 export function mixinBg<T extends Constructor>(base: T): Constructor<CanBg> & T {
   return class extends base {
-    private _bg: string;
+    _superHyperInternalPropertyBg: string;
 
-    get bg(): string { return this._bg; }
+    get bg(): string { return this._superHyperInternalPropertyBg; }
     set bg(val: string) {
       const defaultColor = val || DEFAULT_BG;
       if (defaultColor !== this.bg) {
-        this._bg = defaultColor;
+        this._superHyperInternalPropertyBg = defaultColor;
       }
     }
 
--- src/lib/src/common/shadow-color.ts
+++ src/lib/src/common/shadow-color.ts
@@ -2,14 +2,18 @@ import { Constructor } from './constructor';
 
 export interface CanShadowColor {
   shadowColor: string;
+  /**
+   * It is only used for common behavior, therefore, it should not be used for other purposes.
+   */
+  readonly _superHyperInternalPropertyShadowColor: string;
 }
 
 export function mixinShadowColor<T extends Constructor>(base: T): Constructor<CanShadowColor> & T {
   return class extends base {
-    private _shadowColor: string;
+    _superHyperInternalPropertyShadowColor: string;
 
-    get shadowColor(): string { return this._shadowColor; }
-    set shadowColor(value: string) { this._shadowColor = value; }
+    get shadowColor(): string { return this._superHyperInternalPropertyShadowColor; }
+    set shadowColor(value: string) { this._superHyperInternalPropertyShadowColor = value; }
 
     constructor(...args: any[]) { super(...args); }
   };
