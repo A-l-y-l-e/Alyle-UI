6ce2d065cce2d80071ed105e7505ef2ee80a95f7
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2019-04-23 01:26:03 -0400

    update CI

--- ci/scripts/prepare.sh
+++ ci/scripts/prepare.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-echo github
+# github
 COMMIT_AUTHOR_NAME=$(git --no-pager show -s --format='%an' HEAD)
 COMMIT_AUTHOR_EMAIL=$(git --no-pager show -s --format='%ae' HEAD)
 echo "https://${GITHUB_USER_TOKEN}:@github.com" > .git/credentials
 git config --global user.name "${COMMIT_AUTHOR_NAME}"
 git config --global user.email "${COMMIT_AUTHOR_EMAIL}"
 git config --global credential.helper "store --file=.git/credentials"
-echo npm
+
+# npm
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 npm config set @alyle:registry https://registry.npmjs.org
 git clone https://github.com/A-l-y-l-e/alyle-ui-builds.git --depth 3
