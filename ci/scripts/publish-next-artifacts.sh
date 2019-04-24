74dd04ab1ca48d1d756a8c211f61be88748198a0
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2019-04-24 16:58:28 -0400

    update CI

---
+++ ci/scripts/publish-next-artifacts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/A-l-y-l-e/alyle-ui-builds.git
+git clone https://github.com/A-l-y-l-e/Alyle-UI.git
+
+cd Alyle-UI
+
+PACKAGE_VERSION=$(node -p -e "require('./alyle-ui-builds/package.json').version")
+
+# github
+COMMIT_MESSAGE=$(git show -s --format=%s)
+COMMIT_SHA=$(git rev-parse HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+COMMIT_AUTHOR_NAME=$(git --no-pager show -s --format='%an' HEAD)
+COMMIT_AUTHOR_EMAIL=$(git --no-pager show -s --format='%ae' HEAD)
+
+cd ..
+
+
+echo "https://${GITHUB_USER_TOKEN}:@github.com" > .git/credentials
+git config --global user.name "${COMMIT_AUTHOR_NAME}"
+git config --global user.email "${COMMIT_AUTHOR_EMAIL}"
+git config --global credential.helper "store --file=.git/credentials"
+
+cp -r alyle-ui-pkg/ alyle-ui-builds/
+
+cd alyle-ui-builds
+
+git add -A
+git commit -m "${COMMIT_MESSAGE}" -m "${COMMIT_SHA}"
+git tag $PACKAGE_VERSION
+git push origin master --tags
