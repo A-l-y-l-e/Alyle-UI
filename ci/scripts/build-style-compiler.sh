2fa01a1fc4b914e48ffe057fb4939844816f187e
Enlcxx <Enlcxx.es@gmail.com>
2019-11-12 17:02:56 -0500

    ­

--- ci/scripts/build-style-compiler.sh
+++ ci/scripts/build-style-compiler.sh
@@ -5,12 +5,13 @@ set -u -e -o pipefail
 readonly pkg_dir=dist/@alyle/style-compiler
 readonly lib_dir=dist/lib/style-compiler
 
+rm -rf ${pkg_dir}
 yarn tsc -p ${lib_dir}/tsconfig-build.json
 
 mv ${pkg_dir}/src/* ${pkg_dir}
-mv ${pkg_dir}/style-compiler/** ${pkg_dir}/
+cp -r ${pkg_dir}/style-compiler/** ${pkg_dir}/
 
-sed -i -e 's/..\/src\/parse\"/.\/parse/' ${pkg_dir}/compiler.js
+sed -i -e 's/..\/src\/parse\"/.\/parse\"/' ${pkg_dir}/compiler.js
 rm -rf ${pkg_dir}/color
 rm -rf ${pkg_dir}/src
 rm -rf ${pkg_dir}/style-compiler
