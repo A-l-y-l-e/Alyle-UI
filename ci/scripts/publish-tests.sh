32ac06c642a5a382f455a40bad6de35dec5ddbdd
Enlcxx <Enlcxx.es@gmail.com>
2019-11-13 09:34:48 -0500

    ­

--- ci/scripts/publish-tests.sh
+++ ci/scripts/publish-tests.sh
@@ -2,5 +2,6 @@
 
 set -e -o pipefail
 
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 TS_NODE_COMPILER_OPTIONS='{"module":"commonjs"}' node_modules/.bin/ava --tap | txunit > dist/test-results.xml
