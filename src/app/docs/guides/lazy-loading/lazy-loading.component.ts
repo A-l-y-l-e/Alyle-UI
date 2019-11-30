eca1aa7e0f74ae4c466fa34df8f5db9d3f2851dd
Enlcxx <Enlcxx.es@gmail.com>
2019-11-29 20:07:18 -0500

    ­

--- src/app/docs/guides/lazy-loading/lazy-loading.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'aui-lazy-loading',
-  templateUrl: './lazy-loading.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class LazyLoadingComponent { }
