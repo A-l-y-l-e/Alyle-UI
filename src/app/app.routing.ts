e4599c950caf1138f61b5fe414f64ae2f9d1aed8
Enlcxx <Enlcxx.es@gmail.com>
2019-11-29 19:58:21 -0500

    ­

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -24,6 +24,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [EmptyComponent],
+  exports: [EmptyComponent],
   imports: [
     RouterModule.forRoot(routes, {
       initialNavigation: 'enabled',
