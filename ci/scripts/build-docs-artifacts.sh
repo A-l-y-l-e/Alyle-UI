67d05b62b5eb4f8b4787a8370caf6d2e7110268a
Enlcxx <Enlcxx.es@gmail.com>
2019-12-12 20:03:09 -0500

    chore: new release nightly

--- ci/scripts/build-docs-artifacts.sh
+++ ci/scripts/build-docs-artifacts.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 set -u -e -o pipefail
+source ./ci/setup.sh
+
+export MSG=$(ts-node tools/src/commit-msg)
 
 yarn
 yarn tools:prepare-lib
+ts-node tools/*/bump-version.ts
 ./ci/scripts/build-style-compiler.sh
 node dist/@alyle/ui/style-compiler/main dist/lib
 yarn build:@alyle/ui
--- tools/src/bump-version.ts
+++ tools/src/bump-version.ts
@@ -11,12 +11,14 @@ const pkgLib = JSON.parse(readFileSync(`${process.cwd()}/src/lib/package.json`, 
 const styleCompilerLib = JSON.parse(readFileSync(`${process.cwd()}/src/lib/style-compiler/package.json`, 'utf8').toString());
 
 const {
-  NEW_RELEASE
+  MSG
 } = process.env;
 
-if (!NEW_RELEASE) {
+if (/\[new\s?release\]/i.test(MSG!)) {
   console.log('Bump Version: skiped.');
   process.exit(0);
+} else {
+  updateVersion();
 }
 
 function updateVersion() {
@@ -43,18 +45,26 @@ function createVersion(currentVersion: string) {
   const date = `${moment(now).format('YYMMDD')}${moment().format('HHmm')}`;
   let version = '';
 
-  if (/patch/i.test(NEW_RELEASE!)) {
+  if (/patch/i.test(MSG!)) {
     version = semver.inc(currentVersion, 'patch')!;
-  } else if (/nightly/i.test(NEW_RELEASE!)) {
+  } else if (/nightly/i.test(MSG!)) {
     version = semver.inc(currentVersion, 'prerelease', 'nightly')!.replace('ly.0', `ly.${date}`);
-  } else if (/minor/i.test(NEW_RELEASE!)) {
+  } else if (/minor/i.test(MSG!)) {
     version = semver.inc(currentVersion, 'minor')!;
-  } else if (/major/i.test(NEW_RELEASE!)) {
+  } else if (/major/i.test(MSG!)) {
     version = semver.inc(currentVersion, 'major')!;
+  } else {
+    process.exit(0);
+    console.log('Bump Version: skipped.');
+  }
+
+  if (/nightly/.test(MSG!)) {
+    writeFileSync('./ci/scripts/NIGHTLY', version, 'utf8');
+  } else {
+    writeFileSync('./ci/scripts/NEW_RELEASE', version, 'utf8');
   }
 
   console.log(`New version: ${version}`);
-  writeFileSync('./ci/scripts/VERSION', version, 'utf8');
 
   return {
     version,
@@ -62,4 +72,3 @@ function createVersion(currentVersion: string) {
   };
 }
 
-updateVersion();
