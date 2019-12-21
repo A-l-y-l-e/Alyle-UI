e714e8e60312590b0414af2dc5cbe9a9488041f6
Enlcxx <Enlcxx.es@gmail.com>
2019-12-21 13:49:36 -0500

    chore: update Azure Pipelines

---
+++ ci/scripts/write-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+. ci/setup.sh
+
+which ts-node
+export MSG=$(ts-node tools/src/commit-msg)
+export SHA=$(node -pe 'const {env}=process;env.SYSTEM_PULLREQUEST_SOURCECOMMITID || env.BUILD_SOURCEVERSION')
+echo $MSG > ./ci/scripts/MSG
+echo $SHA > ./ci/scripts/SHA
+echo $MSG.$SHA
