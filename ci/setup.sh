13975b89d34dea4ffc28246b4467930cfea046dd
Enlcxx <Enlcxx.es@gmail.com>
2019-12-21 14:01:58 -0500

    ­

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export TS_NODE_COMPILER_OPTIONS='{"module": "commonjs"}'
 export PATH="$PATH:$(pwd)/node_modules/.bin"
