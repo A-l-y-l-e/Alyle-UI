d7bda2b11ba94e0b6e9564aa03d964f03ab32c96
Enlcxx <Enlcxx.es@gmail.com>
2019-12-21 13:22:29 -0500

    test commit

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export TS_NODE_COMPILER_OPTIONS='{"module": "commonjs"}'
 export PATH="$PATH:$(pwd)/node_modules/.bin"
