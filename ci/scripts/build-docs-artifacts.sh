abec23a84b78ba1afffcb30cd2e1b40d93480c5d
Enlcxx <Enlcxx.es@gmail.com>
2020-02-21 17:47:34 -0500

    fix deploy

--- ci/scripts/build-docs-artifacts.sh
+++ ci/scripts/build-docs-artifacts.sh
@@ -4,9 +4,6 @@ set -u -e -o pipefail
 source ./ci/setup.sh
 
 yarn
-export MSG=$(ts-node tools/src/commit-msg)
-echo $MSG
-ts-node tools/*/bump-version.ts
 yarn tools:prepare-lib
 ./ci/scripts/build-style-compiler.sh
 node dist/@alyle/ui/style-compiler/main dist/lib
