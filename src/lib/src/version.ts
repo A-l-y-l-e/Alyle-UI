dbcb81e7447b1db91bda31f5b85727e9bf559ea1
Enlcxx <Enlcxx.es@gmail.com>
2020-03-06 22:01:39 +0000

    release @alyle/ui v3.1.1 🎉
    [ci skip]

--- src/lib/src/version.ts
+++ src/lib/src/version.ts
@@ -1,2 +1,2 @@
-export const AUI_VERSION = '3.1.0';
-export const AUI_LAST_UPDATE = '2020-03-06T01:53:03.479Z';
+export const AUI_VERSION = '3.1.1';
+export const AUI_LAST_UPDATE = '2020-03-06T21:58:09.772Z';
