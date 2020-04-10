a04680477afc61087321bd2897b59653f51f1ef1
Enlcxx <Enlcxx.es@gmail.com>
2020-04-09 20:27:46 -0500

    fix tests

--- src/lib/schematics/ng-add/index.test.ts
+++ src/lib/schematics/ng-add/index.test.ts
@@ -59,7 +59,7 @@ test(`ng-add-setup-project with default options`, async t => {
   t.is(appModule.match(/LY_THEME_NAME/g)?.length, 2);
 
   t.is(appComponent.match(/StyleRenderer/g)?.length, 3);
-  t.true(appComponent.includes('readonly sRendeder: StyleRenderer'));
+  t.true(appComponent.includes('readonly sRenderer: StyleRenderer'));
   t.true(appComponent.includes('readonly classes = this.sRenderer.renderSheet(STYLES, true)'));
 
   t.true(main.includes(`import 'hammerjs';`));
