213f0e24a65dcfa6252f11fc5e22604a85ee64f1
Enlcxx <Enlcxx.es@gmail.com>
2020-04-09 20:18:08 -0500

    ­

--- ci/scripts/build-docs-artifacts.sh
+++ ci/scripts/build-docs-artifacts.sh
@@ -5,9 +5,12 @@ source ./ci/setup.sh
 
 yarn
 yarn tools:prepare-lib
+
 ./ci/scripts/build-style-compiler.sh
 node dist/@alyle/ui/style-compiler/main dist/lib
+
 yarn build:@alyle/ui
 ./ci/scripts/build-style-compiler.sh
+
 ts-node tools/*/fix-style-compiler.ts
 ls dist -lh
