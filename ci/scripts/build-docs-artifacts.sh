37df421a8b0d4f619d4fa28a7a168bd26182aa47
Enlcxx <Enlcxx.es@gmail.com>
2020-04-11 14:26:35 -0500

    fix ci

--- ci/scripts/build-docs-artifacts.sh
+++ ci/scripts/build-docs-artifacts.sh
@@ -9,7 +9,11 @@ yarn tools:prepare-lib
 ./ci/scripts/build-style-compiler.sh
 node dist/@alyle/ui/style-compiler/main dist/lib
 
-yarn build:@alyle/ui
+yarn ng-packagr -p dist/lib/ng-package.json
+yarn build:schematics
+ts-node tools/src/build-lib
+ts-node tools/src/copy-readme
+
 ./ci/scripts/build-style-compiler.sh
 
 ts-node tools/*/fix-style-compiler.ts
--- tools/src/build-lib.ts
+++ tools/src/build-lib.ts
@@ -1,19 +1,19 @@
-const replace = require('replace-in-file');
-
+import * as _replace from 'replace-in-file';
+const replace: typeof _replace.default = require('replace-in-file');
 
 // fix typings
 const changes = replace.sync({
   files: 'dist/@alyle/ui/**/*.d.ts',
   from: /(:?\.\.\/)+@alyle\/ui/g,
   to: '@alyle/ui'
-});
+}).filter(({ hasChanged }) => hasChanged);
 
 // fix path
 const changesSchematics = replace.sync({
   files: 'dist/@alyle/ui/schematics/**/*.js',
   from: /require\(\"\@schematics\/angular\/node_modules\/typescript\"\)/g,
   to: 'require("typescript")'
-});
+}).filter(({ hasChanged }) => hasChanged);
 
 console.log('fix typings', { changes });
 console.log('fix path', { changesSchematics });
