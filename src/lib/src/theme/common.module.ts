174d179e8eae4efda8b1be231737a693d987dbe8
Enlcxx <Enlcxx.es@gmail.com>
2020-08-06 14:28:54 -0500

    *

--- src/lib/src/theme/common.module.ts
+++ src/lib/src/theme/common.module.ts
@@ -5,7 +5,7 @@ import { LyWithClass } from './with-class.directive';
 import { LyStyle, LyStyleDeprecated } from './style.directive';
 
 @NgModule({
-  declarations: [LyStyle, LyWithClass, LyPaper],
+  declarations: [LyStyle, LyWithClass, LyPaper, LyStyleDeprecated],
   exports: [LyStyle, LyWithClass, LyPaper, LyStyleDeprecated]
 })
 export class LyCommonModule { }
