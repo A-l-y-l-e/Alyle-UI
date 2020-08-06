5cae33fee2dd250f0700650b0a35bf8673d75305
Enlcxx <Enlcxx.es@gmail.com>
2020-08-06 17:15:03 -0500

    release @alyle/ui@8.0.1 🎉

--- src/lib/src/version.ts
+++ src/lib/src/version.ts
@@ -1,2 +1,2 @@
-export const AUI_VERSION = '8.0.0';
-export const AUI_LAST_UPDATE = '2020-08-06T20:27:01.517Z';
+export const AUI_VERSION = '8.0.1';
+export const AUI_LAST_UPDATE = '2020-08-06T21:58:50.622Z';
