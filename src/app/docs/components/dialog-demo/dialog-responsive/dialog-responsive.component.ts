8d232cdc5bd870ed54655448c115cb9ddb250443
Enlcxx <Enlcxx.es@gmail.com>
2020-08-14 18:35:57 -0500

    update docs

--- src/app/docs/components/dialog-demo/dialog-responsive/dialog-responsive.component.ts
+++ src/app/docs/components/dialog-demo/dialog-responsive/dialog-responsive.component.ts
@@ -1,33 +1,42 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { ThemeVariables, LyTheme2 } from '@alyle/ui';
+import { ThemeVariables, lyl, StyleRenderer } from '@alyle/ui';
 import { LyDialogRef, LyDialog } from '@alyle/ui/dialog';
 
-const STYLES_DIALOG = (theme: ThemeVariables) => ({
-  width: '320px',
-  [theme.getBreakpoint('Small')]: {
-    borderRadius: 0,
-    width: '100vw',
-    height: '100vh',
-    maxWidth: '100vw !important',
-    maxHeight: '100vh !important'
-  }
-});
+const STYLES = (theme: ThemeVariables) => {
+  return {
+    dialog: lyl `{
+      @media ${theme.breakpoints['Small']},
+      ${theme.breakpoints['XSmall']} {
+        border-radius: 0
+      }
+    }`
+  };
+};
 
 @Component({
   selector: 'aui-dialog-responsive',
   templateUrl: './dialog-responsive.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    StyleRenderer
+  ]
 })
 export class DialogResponsiveComponent {
 
+  readonly classes = this.sRenderer.renderSheet(STYLES);
+
   constructor(
     private _dialog: LyDialog,
-    private _theme: LyTheme2
+    private sRenderer: StyleRenderer
   ) { }
 
   open() {
     const dialogRef = this._dialog.open<DialogResponsiveDemo>(DialogResponsiveDemo, {
-      containerClass: this._theme.style(STYLES_DIALOG)
+      width: [320, '100vw@XSmall@Small'],
+      height: '100vh@XSmall@Small',
+      maxWidth: '100vw@XSmall@Small',
+      maxHeight: '100vh@XSmall@Small',
+      containerClass: this.classes.dialog
     });
     dialogRef.afterClosed.subscribe((result) => console.log(result));
   }
