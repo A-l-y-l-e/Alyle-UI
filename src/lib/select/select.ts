7e39eb443a65cd3abd14f261a6d0f81e0625f98f
Enlcxx <Enlcxx.es@gmail.com>
2020-09-19 13:48:09 -0500

    fix some bugs

--- src/lib/select/select.ts
+++ src/lib/select/select.ts
@@ -68,7 +68,7 @@ import { FocusableOption, FocusOrigin, ActiveDescendantKeyManager } from '@angul
 import { ENTER, SPACE, hasModifierKey, DOWN_ARROW, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, A } from '@angular/cdk/keycodes';
 import { coerceNumberProperty, coerceBooleanProperty } from '@angular/cdk/coercion';
 import { SelectionModel } from '@angular/cdk/collections';
-import { Style, WithStyles } from '../src/minimal';
+import { Style, WithStyles } from '@alyle/ui';
 import { getLySelectNonFunctionValueError, getLySelectNonArrayValueError } from './select-errors';
 
 
