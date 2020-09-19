5cf438666e3f42efb7fd5e7740ca6afc02d4ba65
Enlcxx <Enlcxx.es@gmail.com>
2020-09-19 17:39:24 -0500

    update docs

--- src/app/docs/layout/tabs-demo/basic-tabs/basic-tabs.component.ts
+++ src/app/docs/layout/tabs-demo/basic-tabs/basic-tabs.component.ts
@@ -1,62 +1,8 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { lyl, ThemeVariables, ThemeRef, StyleRenderer, WithStyles, keyframesUniqueId } from '@alyle/ui';
-import { STYLES as TABS_STYLES } from '@alyle/ui/tabs';
-
-const STYLES = (_theme: ThemeVariables, ref: ThemeRef) => {
-  ref.renderStyleSheet(TABS_STYLES);
-  const tabs = ref.selectorsOf(TABS_STYLES);
-  const fadeIn = keyframesUniqueId.next();
-  const fadeOut = keyframesUniqueId.next();
-  return {
-    $global: lyl `{
-      @keyframes ${fadeIn} {
-        0% {
-          opacity: 0
-        }
-        50% {
-          opacity: 0
-        }
-        100% {
-          opacity: 1
-        }
-      }
-
-      @keyframes ${fadeOut} {
-        0% {
-          opacity: 1
-        }
-        50% {
-          opacity: 0
-        }
-        100% {
-          opacity: 0
-        }
-      }
-    }`,
-    root: lyl `{
-      ${tabs.content} {
-        animation: ${fadeOut} 0.5s
-        opacity: 0
-      }
-      ${tabs.contentActive} {
-        animation: ${fadeIn} 0.5s
-        opacity: 1
-      }
-    }`
-  };
-};
 
 @Component({
   selector: 'aui-basic-tabs',
   templateUrl: './basic-tabs.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    StyleRenderer
-  ]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BasicTabsComponent implements WithStyles {
-  readonly classes = this.sRenderer.renderSheet(STYLES, true);
-  constructor(
-    readonly sRenderer: StyleRenderer
-  ) { }
-}
+export class BasicTabsComponent { }
