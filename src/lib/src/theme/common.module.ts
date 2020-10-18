2606bd84bb4def963b19e0a05507d9e1c99402b9
Enlcxx <Enlcxx.es@gmail.com>
2020-10-18 16:44:31 -0500

    update docs

--- src/lib/src/theme/common.module.ts
+++ src/lib/src/theme/common.module.ts
@@ -4,15 +4,15 @@ import { LyPaper } from './paper';
 import { LyWithClass } from './with-class.directive';
 import { LyStyle, LyStyleDeprecated } from './style.directive';
 
-/**
- * @deprecated
- */
 @NgModule({
   declarations: [LyStyle, LyWithClass, LyPaper],
   exports: [LyStyle, LyWithClass, LyPaper]
 })
 export class LyCommonModule { }
 
+/**
+ * @deprecated
+ */
 @NgModule({
   exports: [LyStyleDeprecated],
   declarations: [LyStyleDeprecated]
