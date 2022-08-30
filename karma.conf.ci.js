4c5a258f5c13d962fcd5d6f412defb318ff8eb0b
Enlcxx <enlcxx.es@gmail.com>
2022-08-29 19:26:38 -0500

    add cobertura

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -30,7 +30,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'cobertura' }
       ]
     },
     reporters: ['progress', 'kjhtml', 'junit'],
