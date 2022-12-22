afdd13aea33baf787b22917db6378f46a3529072
Enlcxx <enlcxx.es@gmail.com>
2022-12-22 11:06:45 -0500

    Update setup.sh

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,4 +1,2 @@
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-export TS_NODE_COMPILER_OPTIONS='{"module": "commonjs"}'
-export TS_NODE_PROJECT='tsconfig.json'
 export PATH="$PATH:$(pwd)/node_modules/.bin"
