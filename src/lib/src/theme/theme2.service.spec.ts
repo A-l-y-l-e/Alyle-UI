2b852199da1bc55240b1b5b039282d49108fcb0d
Enlcxx <enlcxx.es@gmail.com>
2023-06-09 17:32:14 -0500

    fix ng test

--- src/lib/src/theme/theme2.service.spec.ts
+++ src/lib/src/theme/theme2.service.spec.ts
@@ -1,15 +1,59 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AUIThemeModule } from '@testing/theme.module';
 
-import { Theme2Service } from './theme2.service';
+import { lyl } from '../parse';
+import { LyTheme2 } from './theme2.service';
 
-describe('Theme2Service', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [Theme2Service]
-    });
+describe('MyComponent', () => {
+  let component: MyComponent;
+  let fixture: ComponentFixture<MyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        AUIThemeModule,
+      ],
+      declarations: [ MyComponent ]
+    })
+    .compileComponents();
   });
 
-  it('should be created', inject([Theme2Service], (service: Theme2Service) => {
-    expect(service).toBeTruthy();
-  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('add style containers sorted by priority', () => {
+    const containerStyleList: Element[] = [];
+    document.body.querySelectorAll('ly-s-c').forEach(child => {
+      containerStyleList.push(child);
+    });
+    expect(containerStyleList.length)
+      .withContext('count 4 style containers')
+      .toEqual(4);
+    expect(containerStyleList.map(item => Number(item.getAttribute('priority')!)).join(' '))
+      .withContext('the styles should be sorted by priority')
+      .toEqual([-2, -1, -0.5, 0].join(' '));
+  });
 });
+
+@Component({
+  template: ` `
+})
+class MyComponent {
+  constructor(
+    theme: LyTheme2
+  ) {
+    [-0.5, -1, -2, 0].forEach(priority => {
+      theme.renderStyle(`${Math.random()}`, () => lyl `{
+        color: rgb(0, 0, 0)
+      }`, priority);
+    });
+  }
+}
