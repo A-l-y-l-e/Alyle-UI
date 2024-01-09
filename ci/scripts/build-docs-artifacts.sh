69d9eb658712682813d36ea326627e52c5468edc
Enlcxx <enlcxx.es@gmail.com>
2024-01-09 22:32:20 +0000

    fix build

--- ci/scripts/build-docs-artifacts.sh
+++ ci/scripts/build-docs-artifacts.sh
@@ -9,7 +9,7 @@ yarn tools:prepare-lib
 ./ci/scripts/build-style-compiler.sh
 node dist/@alyle/ui/style-compiler/main dist/lib
 
-yarn ng-packagr -p dist/lib/ng-package.json
+yarn ng-packagr --config dist/lib/tsconfig.lib.json -p dist/lib/ng-package.json
 yarn build:schematics
 ts-node --esm --project ./tools/tsconfig.tn.json tools/src/build-lib.ts
 ts-node --esm --project ./tools/tsconfig.tn.json tools/src/copy-readme.ts
