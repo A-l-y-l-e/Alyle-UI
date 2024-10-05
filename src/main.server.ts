e943d0e6f4dc649e1e355eb709fc3b158490fd6a
Enlcxx <enlcxx.es@gmail.com>
2024-10-05 12:15:53 -0500

    support for Angular 17

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,11 +1,1 @@
-import { enableProdMode } from '@angular/core';
-
-import { environment } from '@env/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-export { AppServerModule } from './app/app.server.module';
-
-
+export { AppServerModule as default } from './app/app.module.server';
